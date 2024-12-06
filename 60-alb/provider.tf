bade3cb46717937d0305b92278b13b71d419f406
Your Name <you@example.com>
2024-12-06 16:50:53 +0530

    msg

--- 60-alb/provider.tf
+++ 60-alb/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   
   backend "s3" {
-    bucket = "aws-remote-state"
+    bucket = "aws-raidi-state"
     key    = "expense-alb-ingress"
     region = "us-east-1"
     dynamodb_table = "aws-locking"
